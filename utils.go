998b2e1221e58af681dfda0f05ee89b5a99b1ce8
Alex Svetkin <alex.svetkin@gmail.com>
2025-07-14 11:30:06 +0200

    added content escaping for comment nodes

--- utils.go
+++ utils.go
@@ -1,5 +1,7 @@
 package elem
 
+import "strings"
+
 // If conditionally renders one of the provided elements based on the condition
 func If[T any](condition bool, ifTrue, ifFalse T) T {
 	if condition {
@@ -15,4 +17,24 @@ func TransformEach[T any](items []T, fn func(T) Node) []Node {
 		nodes = append(nodes, fn(item))
 	}
 	return nodes
+}
+
+// EscapeCommentContents escapes the contents of a comment node to ensure safe rendering according to https://html.spec.whatwg.org/multipage/syntax.html#comments
+func EscapeCommentContents(s string) string {
+	// escape disallowed sequences
+	s = strings.ReplaceAll(s, "<!--", "&lt;!--")
+	s = strings.ReplaceAll(s, "-->", "--&gt;")
+	s = strings.ReplaceAll(s, "--!>", "--!&gt;")
+
+	// comments cannot start or end with specific character sequences
+	if strings.HasPrefix(s, ">") {
+		s = "&gt;" + s[1:]
+	}
+	if strings.HasPrefix(s, "->") {
+		s = "-&gt;" + s[2:]
+	}
+	if strings.HasSuffix(s, "<!-") {
+		s = s[:len(s)-3] + "&lt;!-"
+	}
+	return s
 }
